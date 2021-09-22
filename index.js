68784fc18536150509f0299b67cb03316b7ec52a
Forwindz <dfxxlyc@126.com>
2021-09-22 09:35:32 +0200

    Init commit

---
+++ index.js
@@ -0,0 +1,28 @@
+//创建HTTP服务器
+/*
+//1. 加载http模块
+var http = require('http');
+
+//2. 创建http服务器
+// 参数: 请求的回调, 当有人访问服务器的时候,就会自动调用回调函数
+var server = http.createServer(function (request, response) {
+    console.log('有人访问了服务器')
+
+    //回调数据
+    response.write('Hello, My Love')
+    response.end()
+})
+
+//3. 绑定端口
+server.listen(3030)
+
+//4. 执行
+console.log('执行了3030')*/
+let express = require("express");
+let app = express();
+ 
+app.use(express.static("static"));//关键是这一句，我的目录是html的目录
+ 
+app.listen(8080, ()=>{
+    console.log("服务启动成功。");
+})
