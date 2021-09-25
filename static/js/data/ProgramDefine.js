cc6772f6f27027300b4991ca6bcc91a1157d5e0e
Forwindz <dfxxlyc@126.com>
2021-09-26 00:49:27 +0200

    Adjust structure

--- static/js/data/ProgramDefine.js
+++ static/js/data/ProgramDefine.js
@@ -19,10 +19,88 @@ class TypeBehavior{
     acceptableInput=[];
 }
 
-class ProgramNode{
+class NodeConnectInfo{
+    inputNode =null;
+    inputPort =null;
+    outputNode=null;
+    outputPort=null;
+    constructor(_inputPort,_inputNode,_outputPort,_outputNode){
+        this.inputNode=_inputNode;
+        this.inputPort = _inputPort;
+        this.outputNode = _outputNode;
+        this.outputPort = _outputPort;
+    }
+}
+/**
+ * We do not use templates here,
+ * Because dynamic changes of node is really common in such programming system
+ * For example, the node might change, when we deliver different types of data to the node
+ * Therefore, we put every thing together
+ *      at a cost of memory and running efficiency
+ *      for the flexiblity and Scalability
+ * 
+ * Reference: https://wiki.blender.org/wiki/Source/Nodes/NodeInterfaceFramework
+ */
+class Node{
     name="Node";
     inputPorts=[];
     outputPorts=[];
+
+    constructor(){
+        this.defineNode();
+    };
+
+    defineNode() {
+        
+    }
+
+    addInputPort(_name,_typeName,_defaultValue=null){
+        let port = new InputPort(_name,_typeName,_defaultValue);
+        this.inputPorts[_name]=port;
+    }
+
+    addOutputPort(_name,_typeName){
+        let port = new OutputPort(_name,_typeName);
+        this.outputPorts[_name]=port;
+    }
+
+    /**
+     * When execute, invoke this, return outputs
+     * @param {Array} _inputs 
+     */
+    onExecution(_inputs){
+
+    }
+
+    /**
+     * Fire the event, if the node is connected
+     * @param {NodeConnectInfo, Node} connectInfo 
+     */
+    onConnect(connectInfo, nodeDetail){
+
+    }
+
+    index=0;
+    forwardNodes=[];
+    backwardNodes=[];
+
+    connectNode(connectInfo){
+        //TODO: write connection
+    }
+}
+
+/**
+ * A series of nodes form a graph
+ */
+class NodeGraph{
+    nodes=[];
+    constructor(){
+
+    }
+
+    addNode(_node){
+        this.nodes.push(node);
+    }
 }
 
 /**
@@ -32,20 +110,38 @@ class ProgramNode{
 class Port{
     name='port';
     typeName="int";
+    connection = null;
+
+    constructor(_name,_type){
+        this.name=_name;
+        this.typeName=_type;
+    }
 }
 
 class InputPort extends Port{
     defaultValue=null;
+    constructor(_name,_type){
+        super(_name,_type);
+    }
+    constructor(_name,_type,_defaultValue){
+        super(_name,_type);
+        defaultValue=_defaultValue
+    }
+    //TODO: function: set Connection
 }
 
 class OutputPort extends Port{
+    constructor(_name,_type){
+        super(_name,_type);
+    }
+    //TODO: function: set Connection
 }
 
 /**
- * Manage information from data
+ * Manage information for all defined nodes and types
  */
 class DefineManager{
     nodeID=[];
     typeID=[];
-
+    //TODO: complete this
 }
---
+++ static/js/data/ProgramUIData.js
@@ -0,0 +1,11 @@
+
+/**
+ * This file define some data structure for the Node system (UI)
+ * 
+ */
+
+class NodeUITemplate{
+    dataRef = null; //reference to ProgramDefine's data
+    color = null;
+    
+}
